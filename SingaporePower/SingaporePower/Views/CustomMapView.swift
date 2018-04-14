d5036f7f9fa99307441982cda8b8fe4bcde4f17d
VK <vaisakh.krishnan90@gmail.com>
2018-04-14 21:33:21 +0800

    Annotation and anotation titles added in map view

--- SingaporePower/SingaporePower/Views/CustomMapView.swift
+++ SingaporePower/SingaporePower/Views/CustomMapView.swift
@@ -17,6 +17,7 @@ class CustomMapView: MKMapView ,MKMapViewDelegate {
         let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 1.35735, longitude: 103.82)
         let region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate,MKCoordinateSpanMake(0.4, 0.4))
         self.setRegion(region, animated: true)
+        self.delegate = self
     }
     
     //MARK:- Populate Anotations
@@ -25,7 +26,8 @@ class CustomMapView: MKMapView ,MKMapViewDelegate {
         for i in 0...data.count-1 {
             let psiModel = data[i]
             let tempAnotation = MKPointAnnotation()
-            
+            tempAnotation.title = psiModel.name
+            tempAnotation.subtitle = "Vaisakh krishnan Poovathummoottil House Pezhumpar p o Vadassrikara"
             tempAnotation.coordinate = CLLocationCoordinate2D(latitude: psiModel.latittude, longitude: psiModel.longittude)
             anotationArray.append(tempAnotation)
         }
@@ -34,5 +36,32 @@ class CustomMapView: MKMapView ,MKMapViewDelegate {
         }
     }
     
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        
+    }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "") {
+            annotationView.annotation = annotation
+            return annotationView
+        } else {
+            let annotationView = MKPinAnnotationView(annotation:annotation, reuseIdentifier:"")
+            annotationView.isEnabled = true
+            annotationView.canShowCallout = true
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView.rightCalloutAccessoryView = btn
+            return annotationView
+        }
+    }
+    
+//    override func view(for annotation: MKAnnotation) -> MKAnnotationView? {
+//        let pinAnotation:MKPinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "loc")
+//        return pinAnotation
+//    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+    }
+    
+
 }
